60370069e027e40fff11ea3c429a6c3abe38a6ca
Matheus Eduardo Leite Marcolino <matheuselm@ciandt.com>
2022-04-26 14:23:11 -0300

    update escaneiaArquivo

--- ex04.py
+++ ex04.py
@@ -1,19 +1,31 @@
 from inspect import _void
 from opcode import opname
 
-#Código principal
-def main():
-    fileName = input("Insira o nome do arquivo de teste: ")
+#Função responsável por ler o arquvio
+def escaneiaArquivo(fileName):
+    x = 0; y = 0; linha = ""; comando = ""
     try:
         file = open(fileName, "r")
     except:
         print("Arquivo não encontrado")
-    escaneiaArquivo(file)
-    
-if __name__ == "__main__":
-    main()
-
-#Função responsável por ler o arquvio
-def escaneiaArquivo(arquivo):
-    #inserir comandos de leitura
+    linha = file.readline()
+    while(linha != ""):
+       linha.strip()
+       if(linha != "inicio:" or linha != "fim."):
+           if(linha[0] == 'x'):
+               x = linha[2]
+           elif(linha[0] == "y"):
+                y = linha[2]
+           else:
+               comando = linha[2:linha.rfind(";")]
+       else:
+           break
+       linha = file.readline()
     return _void
+
+def substituiComando():
+    return _void
+
+#Código principal
+nome = input("Insira o nome do arquivo de teste: ")
+teste = escaneiaArquivo(nome)
